7a1ac9ae9d5901e263ecf7a278e3c1b10e0920ca
Kemo <136203274+kemo-1@users.noreply.github.com>
2024-12-02 11:04:55 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 
 export default {
-base: '/tvt',
-    plugins: [gleam(), VitePWA({
-        workbox: {
-            globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-        }, injectRegister: 'script-defer', registerType: 'autoUpdate'
-    })
+    base: '/tvt',
+    plugins: [gleam()
     ],
 }
